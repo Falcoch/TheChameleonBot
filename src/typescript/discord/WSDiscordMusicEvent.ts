cf4c1406eaa1ed0901439dc77ec951949b0584e1
Falcoch <f1lcoch@gmail.com>
2021-12-19 00:52:55 +0100

    Add WSDiscord event and WSDiscordMusic event

---
+++ src/typescript/discord/WSDiscordMusicEvent.ts
@@ -0,0 +1,11 @@
+enum WSDiscordMusicEvent {
+    CHANNEL_EMPTY = "channelEmpty",
+    SONG_ADD = "songAdd",
+    PLAYLIST_ADD = "playlistAdd",
+    QUEUE_DESTROYED = "queueDestroyed",
+    QUEUE_END = "queueEnd",
+    SONG_CHANGED = "songChanged",
+    SONG_FIRST = "songFirst",
+    CLIENT_DISCONECT = "clientDisconnect",
+    ERROR = "error"
+}
